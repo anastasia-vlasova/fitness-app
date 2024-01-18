733d78765e57f612e7bd2153d542a7e92cfc1e36
anastasia-vlasova <122757695+anastasia-vlasova@users.noreply.github.com>
2024-01-18 23:50:39 +0100

    Update settings.py

--- project5/settings.py
+++ project5/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ra4rli35%-fpnojcvehrs*@&#kql)_z2%eil04$c8_zi@*3vde
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'project5.urls'
@@ -123,7 +124,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend', 'build', 'static')]
 
